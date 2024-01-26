6c1116f4b642049b97ecdee7cbc641169580bcb6
MatheusDEVQA <matheus_muniz3@hotmail.com.br>
2024-01-26 16:16:20 -0300

    First commit

---
+++ src/main/java/com/matheus/devqa/matheusspringtestudemy/StartUp.java
@@ -0,0 +1,13 @@
+package com.matheus.devqa.matheusspringtestudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatheusSpringTestUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatheusSpringTestUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheus/devqa/matheusspringtestudemy/MatheusSpringTestUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.devqa.matheusspringtestudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatheusSpringTestUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
