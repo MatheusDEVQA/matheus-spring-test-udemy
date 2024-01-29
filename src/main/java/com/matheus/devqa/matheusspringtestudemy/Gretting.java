4024f8522178fca918aa53383967a5ca2b7647a6
MatheusDEVQA <matheus_muniz3@hotmail.com.br>
2024-01-29 20:48:18 -0300

    Add Controller gretting

---
+++ src/main/java/com/matheus/devqa/matheusspringtestudemy/Gretting.java
@@ -0,0 +1,22 @@
+package com.matheus.devqa.matheusspringtestudemy;
+
+public class Gretting {
+
+    private final long id;
+    private final String content;
+
+    public Gretting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
