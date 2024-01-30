f5237216b4d87552511f2167308279c18f44ee43
MatheusDEVQA <matheus_muniz3@hotmail.com.br>
2024-01-29 21:25:06 -0300

    Create PErson and Person COntroller

---
+++ src/main/java/controllers/PersonController.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import com.matheus.devqa.matheusspringtestudemy.Gretting;
+import model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import services.PersonServices;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonServices services;
+
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(
+            @PathVariable(value = "id") String id) throws Exception{
+        return services.findById(id);
+    }
+
+}
